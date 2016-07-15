2e303b49d9e81a9b03410c8c8b759b71e17cf968
Anup Nivargi <anupnivargi@gmail.com>
2016-07-15 15:36:25 +0530

    Use hardcoded pool_id for now

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -6,6 +6,7 @@ describe 'Application' do
     #TODO fetch from fixtures
     @cluster_id = 'd74371ad-c292-4ccd-949c-d48de9472afd'
     @osd_id = '371e9305-a17c-4314-bc4a-849098a95f96' 
+    @pool_id = 0
   end
 
   it 'pong' do
@@ -54,7 +55,7 @@ describe 'Application' do
     end
 
     it 'details' do
-      get "/clusters/#{@cluster_id}/pools/#{@osd_id}", { "CONTENT_TYPE" => "application/json" }
+      get "/clusters/#{@cluster_id}/pools/#{@pool_id}", { "CONTENT_TYPE" => "application/json" }
       expect(last_response.status).to eq 200
       #body = JSON.parse(last_response.body)
     end
